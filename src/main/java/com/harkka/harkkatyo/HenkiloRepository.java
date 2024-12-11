3f71df6528dfc825e50d01ce5998bbd438a3d07f
rikuqt <riku.tomann@seamk.fi>
2024-12-11 14:55:58 +0200

    n+1 kyselyt parantelu

--- src/main/java/com/harkka/harkkatyo/HenkiloRepository.java
+++ src/main/java/com/harkka/harkkatyo/HenkiloRepository.java
@@ -1,5 +1,6 @@
 package com.harkka.harkkatyo;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +10,7 @@ public interface HenkiloRepository extends JpaRepository<Henkilo, Long> {
     List<Henkilo> findHenkilotByTapahtumatId(Long tapahtumaId);
 
     Optional<Henkilo> findByNimi(String nimi);
+
+    @EntityGraph(attributePaths = {"tapahtumat"})
+    List<Henkilo> findAll();
 }
--- src/main/java/com/harkka/harkkatyo/TapahtumaRepository.java
+++ src/main/java/com/harkka/harkkatyo/TapahtumaRepository.java
@@ -1,9 +1,13 @@
 package com.harkka.harkkatyo;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface TapahtumaRepository extends JpaRepository<Tapahtuma, Long> {
     List<Tapahtuma> findTapahtumatByHenkilotId(Long henkiloId);
+
+    @EntityGraph(attributePaths = {"henkilot"})
+    List<Tapahtuma> findAll();
 }
